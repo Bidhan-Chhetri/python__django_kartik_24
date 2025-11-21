abf62477c34b5864a91c50066b6fe9a6b466c202
Bidhan Chhetri <bidhanchhetri52@gmail.com>
2025-11-21 21:15:07 +0545

    nesting the dictionary

---
+++ day6_python/nested_dictionary.py
@@ -0,0 +1,10 @@
+book = {
+        "title": "Python Basic",
+        "author": "John Doe",
+        "details": {
+                    "pages": 250,
+                    "publisher": "Tech press"
+                    }
+        }
+
+print(book["details"]["publisher"])
